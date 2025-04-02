c0a4221ed196848852defe39cc723775b93fca1f
Ssekajja Wavamuno Isaac <ssekajjawavamuno@gmail.com>
2025-04-02 22:13:48 +0300

    Add test case for 10

--- test/test.js
+++ test/test.js
@@ -10,4 +10,7 @@ describe("Fibonacci function tests for valid integers", () => {
   it("Should return 1 for fibonacci(1)", () => {
     assert.equal(fibonacci(1), 1);
   });
+  it("Should return 55 for fibonacci(10)", () => {
+    assert.equal(fibonacci(10), 55);
+  });
 });
