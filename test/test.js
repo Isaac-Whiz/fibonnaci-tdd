17c23fefa9efda8e6e22b831654c09344f49bd0e
Ssekajja Wavamuno Isaac <ssekajjawavamuno@gmail.com>
2025-04-02 21:20:57 +0300

    Initial commit of a failing test.

---
+++ test/test.js
@@ -0,0 +1,9 @@
+// Lorna Naula & Ssekajja Wavamuno Isaac
+import { describe, it } from "mocha";
+import { assert } from "chai";
+
+describe("Fibonacci Function", () => {
+  it("Should return 0 for fibonacci(0)", () => {
+    assert.equal(fibonacci(0), 0);
+  });
+});
